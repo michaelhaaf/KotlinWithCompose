736c373facbe6b0178b57bd28afd5d455bdec0cd
Michael Haaf <michael.haaf@gmail.com>
2023-12-04 03:50:49 -0500

    Gradle improvements (#5)
    * Upgrade gradle wrapper, optimize gradle build parameters
    
    * Automate gradle enterprise Build Scan agreement
    
    * add spotless plugin to gradle for improved linting/formatting
    
    * refactor: run spotlessApply

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
     id("com.google.gms.google-services")
+    id("com.diffplug.spotless") version "6.23.3"
 }
 
 android {
@@ -28,7 +29,7 @@ android {
             isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
         getByName("debug") {
@@ -53,12 +54,23 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    spotless {
+        kotlin {
+            ktfmt()
+            ktlint()
+            diktat()
+            prettier()
+        }
+        kotlinGradle {
+            ktlint()
+            diktat()
+        }
+    }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
-//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    // implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -76,7 +88,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.2")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-//    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+    // implementation("com.firebaseui:firebase-ui-auth:7.2.0")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx:24.6.0")
 }
